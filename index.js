70e8f1bf6818e5e77992d3fb72e4b3bb51f7ca3d
lucasflomuller <lucasflorianomuller@gmail.com>
2018-11-26 23:31:44 +0000

    initial config of express app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+  app = express(),
+  cors = require("cors"),
+  bodyParser = require("body-parser");
+
+// Port and IP config
+const PORT = process.env.PORT || 8081;
+const IP = process.env.IP || '0.0.0.0';
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// All routes here
+app.use(function (req, res, next) {
+  let err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, IP, function () {
+  console.log(`Server is running on IP: ${IP}, PORT: ${PORT}`);
+});
