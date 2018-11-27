5ac5d8691d05421d92c7ccd24ac0fc9651b6b109
lucasflomuller <lucasflorianomuller@gmail.com>
2018-11-27 23:12:35 +0000

    add user middleware for password

--- models/user.js
+++ models/user.js
@@ -19,7 +19,29 @@ const userSchema = new mongoose.Schema({
   profileImageUrl: {
     type: String
   }
-})
+});
+
+userSchema.pre("save", async function(next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 
