f78ecd7ab151d21ae9ccd7c70309fdd68cb08b51
lucasflomuller <lucasflorianomuller@gmail.com>
2018-12-03 01:36:18 +0000

    add additional messages routes (get and delete)

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-const { createMessage } = require("../handlers/messages");
+const { createMessage, getMessage, deleteMessage } = require("../handlers/messages");
 
 // prefix - /api/users/:id/messages
 router.route("/").post(createMessage);
+
+// prefix - api/users/:id/messages/:message_id
+router
+  .route("/")
+  .get(getMessage)
+  .delete(deleteMessage);
 
 module.exports = router;
